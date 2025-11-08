6a2af3837871c5695e45913c78cacdd4cf56ffa7
remysedlak <remysedlak@gmail.com>
2025-11-07 19:27:07 -0500

    version files

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+mod ui;
+mod audio;
+
+use cpal::Stream;
+use ui::MyApp;
+use eframe::egui;
+
+fn main() -> eframe::Result {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "My DAW",
+        options,
+        Box::new(|cc| {
+            // This gives us image support:
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+            Ok(Box::<MyApp>::default())
+        }),
+    )
+}
