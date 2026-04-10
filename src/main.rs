4a001481b84e6167613b4aedc0274550f2bb078e
remysedlak <remysedlak@gmail.com>
2026-04-10 08:05:18 -0400

    add sample player utilizing the hound crate. fix cpal config setup to be less confusing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use cpal::SampleFormat;
+use cpal::StreamConfig;
 use cpal::traits::StreamTrait;
 use cpal::traits::{DeviceTrait, HostTrait};
 
@@ -15,27 +16,37 @@ fn main() {
         .expect("no output device available");
 
     // a config must be defined to use the device properlyz
-    let mut supported_configs_range = device
-        .supported_output_configs()
-        .expect("error while querying configs");
-    let supported_config = supported_configs_range
-        .find(|c| c.sample_format() == SampleFormat::F32)
-        .expect("no f32 config")
-        .with_max_sample_rate();
+    let supported_config = device
+        .default_output_config()
+        .expect("error getting default config");
+
+    let config = supported_config.config();
     let sample_format = supported_config.sample_format();
-    let config = supported_config.into();
+
+    println!("{}", config.sample_rate);
 
     // track sample position and fill buffer
     let mut i: f32 = 0.0;
-    let callback = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+    let sine_callback = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
         for sample in data.iter_mut() {
-            *sample = (2.0 * std::f32::consts::PI * 220.0 * i / config.sample_rate).sin();
+            *sample = (2.0 * std::f32::consts::PI * 220.0 * i / config.sample_rate as f32).sin();
             i += 1.0;
         }
     };
 
+    let mut j: f32 = 0.0;
+    let snare: Vec<f32> = path_to_vector("AttackS.wav");
+    let snare_callback = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+        for sample in data.iter_mut() {
+            if j < snare.len() as f32 {
+                *sample = snare[j as usize];
+                j += 1.0;
+            }
+        }
+    };
+
     let stream = match sample_format {
-        SampleFormat::F32 => device.build_output_stream(&config, callback, err_fn, None),
+        SampleFormat::F32 => device.build_output_stream(&config, snare_callback, err_fn, None),
         sample_format => panic!("Unsupported sample format '{sample_format}'"),
     }
     .expect("Failed to build the output stream.");
@@ -43,4 +54,26 @@ fn main() {
     // start the output stream
     stream.play().expect("Failed to play the output stream.");
     std::thread::sleep(std::time::Duration::from_secs(5));
+}
+
+// load an instrument path into a vector of floats
+pub fn path_to_vector(instrument_path: &str) -> Vec<f32> {
+    // Open the WAV file using the hound library
+    let mut reader = match hound::WavReader::open(instrument_path) {
+        Ok(result) => result,
+        Err(err) => panic!("{}", err),
+    };
+
+    let spec = reader.spec();
+    let divisor = 1 << (spec.bits_per_sample - 1);
+
+    // Read all samples as i32 (32-bit audio)
+    let samples = reader.samples::<i32>();
+
+    // Convert i16 samples to f32 normalized values
+    let vector: Vec<f32> = samples
+        .map(|result| result.unwrap()) // Unwrap each Result<i32>
+        .map(|i32_value| i32_value as f32 / divisor as f32) // Normalize to [-1.0, 1.0]
+        .collect();
+    vector
 }
